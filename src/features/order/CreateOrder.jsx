07a646e93ab947a53867a7bf52d3cdc7e54fc532
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-09-06 21:56:10 +0530

    styled form elements

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import createOrder from "../../services/apiRestaurant";
+
 
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -46,14 +46,14 @@ function CreateOrder() {
       <Form method="POST" action="/order/new">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input " type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
 
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
 
           {formErrors?.phone && <p>{formErrors.phone}</p>}
@@ -63,12 +63,14 @@ function CreateOrder() {
           <label>Address</label>
 
           <div>
-            <input type="text" name="address" required />
+            <input className="input"
+             type="text" name="address" required />
           </div>
         </div>
 
         <div>
           <input
+          className="h-6 w-6 accent-yellow-400 focus:outline-none  focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
