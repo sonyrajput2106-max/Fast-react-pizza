51507a52e2a2f63b9541d0ef087f2309d6c4f95c
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-09-07 23:45:02 +0530

    reused styling

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-
+import { Form, useActionData, useNavigation } from "react-router-dom";
+import Button from "../../ui/Button";
 
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -88,8 +88,9 @@ function CreateOrder() {
             value={JSON.stringify(cart)}
           />
 
-          <button disabled={isSubmitting} className="bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed">
+          <button disabled={isSubmitting} >
             {isSubmitting ? "Placing order..." : "Order now"}
+            <Button />
           </button>
         </div>
       </Form>
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import UserName from "../features/user/username";
 
 function Header() {
   return (
-    <header className ="bg-yellow-400 uppercase px-4 py-3 border-b border-stone-200 sm:px-6 flex items-center justify-between ">
+    <header className ="bg-yellow-400 uppercase px-4 py-3 border-b border-stone-200 sm:px-6 flex items-center justify-between  ">
       <Link to="/" className="tracking-widest">Fast React Pizza Co.</Link>
 
       <SearchOrder />
